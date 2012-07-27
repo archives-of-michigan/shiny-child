9a579bf1ee71de1f064dcf4f176f39dd46cce188
Dorie Schwarz <tumblingblocks@gmail.com>
2012-07-26 20:06:55 -0400

    Adding special field selects for when Death Records is a selected collection.

--- searchCustom/getCollections.js
+++ searchCustom/getCollections.js
@@ -6,6 +6,35 @@ function get_collections_js(){
 	}).done(function( html ) {
 		jQuery("#advanced_max_collections_link").parent().append(html);
 		jQuery("#advanced_max_collections_link").remove();
+		jQuery('.collBoxes').click(function(){
+			deathBoxes();
+		});
+		deathBoxes();//call now on preselected
 	});
 	
+}
+
+function deathBoxes(){
+	
+		var deathCol = false;
+		jQuery('.collBoxes:checked').each(function() {
+			if (jQuery(this).val() == '/p129401coll7'){
+				deathCol = true;	
+			}
+		});
+		
+		if (deathCol){
+			if (jQuery('#rid0_field option').size() < 6){
+				jQuery('.adv_search_domain_dd').each(function() {
+					jQuery(this).append('<option value="title">County</option><option value="subjec">City/village/township</option><option value="creato">Last Name</option><option value="descri">Given Name</option><option value="publis">Birth year</option><option value="contri">Age</option><option value="format">Death Year</option><option value="identi">Father\'s Given</option><option value="source">Father\'s Last</option>');
+				});
+			}
+		}else{
+			jQuery('.adv_search_domain_dd').each(function() {
+			
+				var fieldSelID = '#' + jQuery(this).attr('id');
+				var myOption = fieldSelID + ' option:gt(4)';	
+				jQuery(myOption).remove();
+			});
+		}	
 }
