b84ec879ebc3d1989f33536ec4553c95a7c08263
Jill Arnold <arnoldj1@michigan.gov>
2012-09-18 15:19:11 -0400

    Another shot at code for a links page

--- page-civilwar-links.php
+++ page-civilwar-links.php
@@ -13,10 +13,21 @@ get_header(); ?>
 		</div>
 	
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	<h1><?php the_title(); ?></h1>
-	<ul>
-		<?php wp_list_bookmarks('title_li=&categorize=65'); ?>
-	</ul>
+	<?php
+		$bookmarks = get_bookmarks( array(
+			'orderby' => 'name',
+			'order' => 'ASC',
+			'category' => '65'
+                          ));
+
+		// Loop through each bookmark and print formatted output
+		foreach ( $bookmarks as $bm ) {
+		    echo '<li class="bookmark"><h4><a href="'.$bm->link_url.'" target="'.$bm->link_target.'" rel="'.$bm->link_rel.'">'.$bm->link_name.'</a></h4>';
+		    echo '<p class="description">'.$bm->link_notes.'</p>';
+		    echo '<p class="url">'.$bm->link_url.'</p>';
+		    echo '<div class="details"><ul><li class="share-link"><a class="addthis" title="" rel="" href="http://www.addthis.com/bookmark.php">Share This</a> </li></ul></div></li>';
+		}
+	?>
 <?php endwhile; ?>
 			
 			<?php get_template_part('navigation'); ?>
