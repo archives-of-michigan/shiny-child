158d97c6a4e6e4848693c1db9c0cd426ad7e60b0
Jill Arnold <arnoldj1@michigan.gov>
2012-09-18 15:31:03 -0400

    Almost there

--- page-civilwar-links.php
+++ page-civilwar-links.php
@@ -6,11 +6,16 @@
 
 get_header(); ?>
 	
+<div id="wrapper" class="sidebar-right-wrapper">
+		<div id="wrapper_inner">
+	
 	<div id="wrapper">
-		<div id="civilwar-intro"><a href="http://seekingmichigan.org/civil-war/"></a>
-			<div id="intro" class="text-intro">
-			</div>
+	<div id="civilwar-intro"><a href="http://seekingmichigan.org/civil-war/"></a>
+		<div id="intro" class="text-intro">
 		</div>
+	</div>
+	
+		<div id="page-content" class="two_third">
 	
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 	<?php
@@ -24,15 +29,19 @@ get_header(); ?>
 		foreach ( $bookmarks as $bm ) {
 		    echo '<li class="bookmark"><h4><a href="'.$bm->link_url.'" target="'.$bm->link_target.'" rel="'.$bm->link_rel.'">'.$bm->link_name.'</a></h4>';
 		    echo '<p class="description">'.$bm->link_notes.'</p>';
-		    echo '<p class="url">'.$bm->link_url.'</p>';
-		    echo '<div class="details"><ul><li class="share-link"><a class="addthis" title="" rel="" href="http://www.addthis.com/bookmark.php">Share This</a> </li></ul></div></li>';
+		    echo '<hr class="divider-dotted">'
 		}
 	?>
 <?php endwhile; ?>
 			
 			<?php get_template_part('navigation'); ?>
 
+		</div>
+		<div id="sidebar" class="one_third last sidebar-right">
+			<?php get_sidebar('civil war'); ?>
+		</div>
 		<br class="clear" />
+		</div>
 	</div>
 
 <?php get_footer(); ?>
