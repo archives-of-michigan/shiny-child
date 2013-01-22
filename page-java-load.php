f84c386deb4ff433a96ec2fe56ad89befca56008
Jill Arnold <arnoldj1@michigan.gov>
2013-01-22 15:38:40 -0500

    Added back in "page-content" div

--- page-java-load.php
+++ page-java-load.php
@@ -15,18 +15,16 @@ get_header(); ?>
 		<?php if($e404_options['breadcrumbs']) : ?><div id="breadcrumb"><?php e404_breadcrumbs(); ?></div><?php endif; ?>
 	<?php if((isset($e404_options['main_intro_type']) && $e404_options['main_intro_type'] != 'none') || $e404_options['breadcrumbs']) echo '</div>'; ?>
 	
+	<div id="page-content">
+
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 		<?php if($e404_options['page_titles']) : ?><h2 class="fancy-header"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2><?php endif; ?>
 			<div id="post-<?php the_ID(); ?>" class="page-layout">
 				<?php the_content(); ?>
-				
-				<?php if(!is_attachment() && $e404_options['page_comments']) {
-					comments_template('', true);
-				} ?>
 
 			</div>
 <?php endwhile; ?>
-			
+		</div>	
 	</div>
 
 <?php get_footer(); ?>
