bc3c0825b19e7be94ee3aeb0cde2157054679c79
Jill Arnold <arnoldj1@michigan.gov>
2013-03-20 11:44:32 -0400

    Trying to make teach sidebar appear on learn pages

--- page-teach-rightsb.php
+++ page-teach-rightsb.php
@@ -30,7 +30,7 @@ get_header(); ?>
 
 		</div>
 		<div id="sidebar" class="one_third last sidebar-right">
-			<?php get_sidebar('page'); ?>
+			<?php get_sidebar('blog'); ?>
 		</div>
 		<br class="clear" />
 		</div>
