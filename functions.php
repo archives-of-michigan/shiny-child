ef557e080d19ee0424494fd8a12137e8c647c7d7
Jill Arnold <arnoldj1@michigan.gov>
2013-03-27 11:37:35 -0400

    Another update to hooks

--- functions.php
+++ functions.php
@@ -734,14 +734,14 @@ function e404_comment( $comment, $args, $depth ) {
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
-add_action('woocommerce_before_main_content', 'shiny_child_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'shiny_child_wrapper_end', 10);
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
 
-function shiny_child_wrapper_start() {
+function my_theme_wrapper_start() {
 	echo '<section id="main">';
 }
 
-function shiny_child_wrapper_end() {
+function my_theme_wrapper_end() {
 	echo '</section>';
 }
 
