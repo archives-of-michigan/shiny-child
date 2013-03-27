6c94b4637f18907afcfffb2b8017ebe96b978f6a
Jill Arnold <arnoldj1@michigan.gov>
2013-03-27 11:39:42 -0400

    Change the id name.

--- functions.php
+++ functions.php
@@ -738,7 +738,7 @@ add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
 
 function my_theme_wrapper_start() {
-	echo '<section id="main">';
+	echo '<section id="wrapper">';
 }
 
 function my_theme_wrapper_end() {
