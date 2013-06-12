608211b08e7e1b6faf427cfadd22926e9d6dd5d0
Jill Arnold <arnoldj1@michigan.gov>
2013-06-12 13:28:08 -0400

    Template back to normal

--- woocommerce.php
+++ woocommerce.php
@@ -6,12 +6,17 @@
 
 get_header(); ?>
 
+
+
 	<div id="wrapper" class="sidebar-right-wrapper">
 		<div id="wrapper_inner">
 		
-			<div id="breadcrumb"><a href="http://seekingmichigan.org">Home</a><span>&gt;</span><a href="http://seekingmichigan.org/shop">Shop</a><span>&gt;</span><?php echo get_the_title($ID); ?></div>
+		<?php if((isset($e404_options['main_intro_type']) && $e404_options['main_intro_type'] != 'none') || $e404_options['breadcrumbs']) echo '<div id="head_intro">'; ?>
+			<?php include(OF_FILEPATH.'/main-intro-box.php'); ?>
+			<?php if($e404_options['breadcrumbs']) : ?><div id="breadcrumb"><?php e404_breadcrumbs(); ?></div><?php endif; ?>
+		<?php if((isset($e404_options['main_intro_type']) && $e404_options['main_intro_type'] != 'none') || $e404_options['breadcrumbs']) echo '</div>'; ?>
 			
-		<div id="page-content" class="two_third">
+			<div id="page-content" class="two_third">
 
 		<?php woocommerce_content(); ?>
 			
