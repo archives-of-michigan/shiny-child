b0ab9ddb8b853e34151aabdb397a50e99d7f23a0
Jill Arnold <arnoldj1@michigan.gov>
2013-06-12 14:01:43 -0400

    More tweaking

--- woocommerce.php
+++ woocommerce.php
@@ -8,8 +8,14 @@ get_header(); ?>
 
 	<div id="wrapper" class="sidebar-right-wrapper">
 		<div id="wrapper_inner">
-		
-			<div id="breadcrumb"><a href="http://seekingmichigan.org">Home</a><span>&gt;</span><a href="http://seekingmichigan.org/shop">Shop</a><span>&gt;</span><?php echo get_the_title($ID); ?></div>
+			<div id="head_intro">
+				<div id="intro" class="text-intro">
+					<h1><?php echo get_the_title($ID); ?></h1>
+					<hr class="divider divider-bbottom">
+				</div>
+			
+				<div id="breadcrumb"><a href="http://seekingmichigan.org">Home</a><span>&gt;</span><a href="http://seekingmichigan.org/shop">Shop</a><span>&gt;</span><?php echo get_the_title($ID); ?></div>
+			</div>	
 			
 		<div id="page-content" class="two_third">
 
