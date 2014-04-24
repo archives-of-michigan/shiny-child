072d0c79eb79d5017618d7c9a9c48dd8b5723d40
Jill Arnold <arnoldj1@michigan.gov>
2014-04-24 15:25:02 -0400

    More tweaks.

--- page_ipadlabel.php
+++ page_ipadlabel.php
@@ -5,6 +5,17 @@
  */
 
 ?>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>The Missing Label Project &laquo; Seeking Michigan</title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="http://seekingmichigan.org/wp-content/themes/shiny-child/style.css" />
+<link rel='stylesheet' id='gforms_reset_css-css'  href='http://seekingmichigan.org/wp-content/plugins/gravityforms/css/formreset.css?ver=1.8.7' type='text/css' media='all' />
+<link rel='stylesheet' id='gforms_formsmain_css-css'  href='http://seekingmichigan.org/wp-content/plugins/gravityforms/css/formsmain.css?ver=1.8.7' type='text/css' media='all' />
+<link rel='stylesheet' id='gforms_ready_class_css-css'  href='http://seekingmichigan.org/wp-content/plugins/gravityforms/css/readyclass.css?ver=1.8.7' type='text/css' media='all' />
+<link rel='stylesheet' id='gforms_browsers_css-css'  href='http://seekingmichigan.org/wp-content/plugins/gravityforms/css/browsers.css?ver=1.8.7' type='text/css' media='all' />
+</head>
 <body>
 <div id="main_wrapper">
 <div id="wrapper">
@@ -26,3 +37,4 @@
 	</div>
 </div>
 </body>
+</html>
