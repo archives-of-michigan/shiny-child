ac6240319099acdc950a22028b5843fe7ce952c9
Jill Arnold <arnoldj1@michigan.gov>
2016-12-29 11:10:20 -0500

    Teach breadcrumbs
    firefox is displaying the breadcrumbs in a weird way here is my first
    attempt to fix it

--- page-teach.php
+++ page-teach.php
@@ -9,17 +9,20 @@ get_header(); ?>
 	<div id="wrapper">
 		<div id="wrapper_inner">
 		
-		<div id="breadcrumb"><?php e404_breadcrumbs(); ?></div>
+		  <?php if($e404_options['blog_intro_type'] != 'none' || $e404_options['breadcrumbs']) echo '<div id="head_intro">'; ?>
+			  <?php include(OF_FILEPATH.'/blog-intro-box.php'); ?>
+			  <?php if($e404_options['breadcrumbs']) : ?><div id="breadcrumb"><?php e404_breadcrumbs(); ?></div><?php endif; ?>
+		  <?php if($e404_options['blog_intro_type'] != 'none' || $e404_options['breadcrumbs']) echo '</div>'; ?>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-			<div id="post-<?php the_ID(); ?>" class="page-layout">
-				<?php the_content(); ?>
-
-				<?php if(!is_attachment() && $e404_options['page_comments']) {
-					comments_template('', true);
-				} ?>
-			</div>
-<?php endwhile; ?>
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+						<div id="post-<?php the_ID(); ?>" class="page-layout">
+							<?php the_content(); ?>
+			
+							<?php if(!is_attachment() && $e404_options['page_comments']) {
+								comments_template('', true);
+							} ?>
+						</div>
+			<?php endwhile; ?>
 			
 			<?php get_template_part('navigation'); ?>
         </div>
