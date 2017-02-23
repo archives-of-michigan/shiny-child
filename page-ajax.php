90e4628f941c37bbe18eb10a86a16f884a347edd
Jill Arnold <arnoldj1@michigan.gov>
2017-02-23 12:54:30 -0500

    Removing some bad files and fixing single page no sidebar

--- page-ajax.php
+++ page-ajax.php
@@ -6,25 +6,24 @@
 
 get_header(); ?>
 
-
-
 	<div id="wrapper">
+		<div id="wrapper_inner">
+		
+		<div id="breadcrumb"><?php e404_breadcrumbs(); ?></div>
 	
-	<?php if($e404_options['breadcrumbs']) : ?><div id="breadcrumb"><?php e404_breadcrumbs(); ?></div><?php endif; ?>
-	<?php if((isset($e404_options['main_intro_type']) && $e404_options['main_intro_type'] != 'none') || $e404_options['breadcrumbs']) echo '</div>'; ?>
-	
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-			<div id="post-<?php the_ID(); ?>" class="page-layout">
-				<?php the_content(); ?>
-
-				<?php if(!is_attachment() && $e404_options['page_comments']) {
-					comments_template('', true);
-				} ?>
-			</div>
-<?php endwhile; ?>
+		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+					<div id="post-<?php the_ID(); ?>" class="page-layout">
+						<?php the_content(); ?>
+		
+						<?php if(!is_attachment() && $e404_options['page_comments']) {
+							comments_template('', true);
+						} ?>
+					</div>
+		<?php endwhile; ?>
 			
-			<?php get_template_part('navigation'); ?>
-
+		<?php get_template_part('navigation'); ?>
+	  
+		</div>
 		<br class="clear" />
 	</div>
 
---
+++ page-teach-programs.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template name: Teach page for Programs
+ *
+ */
+
+get_header(); ?>
+	
+	<div id="wrapper">
+		<div id="wrapper_inner">
+		
+		  <div id="breadcrumb"><?php e404_breadcrumbs(); ?></div>
+
+		  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+					  <div id="post-<?php the_ID(); ?>" class="page-layout">
+						  <?php the_content(); ?>
+		  
+						  <?php if(!is_attachment() && $e404_options['page_comments']) {
+							  comments_template('', true);
+						  } ?>
+					  </div>
+		  <?php endwhile; ?>
+			
+			<?php get_template_part('navigation'); ?>
+        </div>
+		<br class="clear" />
+	</div>
+
+<?php get_footer(); ?>
